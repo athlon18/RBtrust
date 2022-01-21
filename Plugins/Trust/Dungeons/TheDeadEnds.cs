0949524b2d01925149305b4f91e0238eefbfe9a6
Manta <manta@bismuth.dev>
2022-01-21 05:06:07 -0500

    Inital 90.1 Dead Ends profile

---
+++ Plugins/Trust/Dungeons/TheDeadEnds.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Trust.Data;
+
+namespace Trust.Dungeons
+{
+    /// <summary>
+    /// Lv. 90.1 The Dead Ends dungeon logic.
+    /// </summary>
+    public class TheDeadEnds : AbstractDungeon
+    {
+        /// <summary>
+        /// Gets zone ID for this dungeon.
+        /// </summary>
+        public new const ZoneId ZoneId = Data.ZoneId.TheDeadEnds;
+
+        /// <inheritdoc/>
+        public override DungeonId DungeonId => DungeonId.TheDeadEnds;
+
+        /// <inheritdoc/>
+        public override async Task<bool> RunAsync()
+        {
+            return false;
+        }
+    }
+}
