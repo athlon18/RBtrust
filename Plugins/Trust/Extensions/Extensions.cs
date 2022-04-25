49aa865eb475eb9e63cb886951ca0777f42604cd
xil64 <bobsam1576@gmail.com>
2022-04-25 01:06:45 -0700

    Extensions / MovementHelpers update

--- Plugins/Trust/Extensions/Extensions.cs
+++ Plugins/Trust/Extensions/Extensions.cs
@@ -7,6 +7,7 @@ using ff14bot.Navigation;
 using ff14bot.Objects;
 using ff14bot.AClasses;
 using ff14bot.Interfaces;
+using ff14bot.Enums;
 using System;
 using System.Diagnostics;
 using System.Collections.Generic;
@@ -22,6 +23,95 @@ namespace Trust.Extensions
     /// </summary>
     internal static class Extensions
     {
+        /* public enum TrustNPC : uint
+        {
+          
+            // Yshtola = 729,
+            Urianger = 1492,
+            Alphinaud = 4130, 
+            Alisaie = 5239,
+            Yshtola = 8378,
+            Ryne = 8889,
+            Minfilia = 8917,
+            Lyna = 8919,
+            AlphinaudAvatar = 11264, 
+            AlisaieAvatar = 11265, 
+            UriangerAvatar = 11267,
+            YshtolaAvatar = 11268, 
+            RyneAvatar = 11269, 
+            EstinienAvatar = 11270,
+            Venat = 10586, 
+            VenatPhantom = 10587,
+            EmetSelch = 10898, 
+            Hythlodaeus = 10899,
+            Thancred = 713,
+            CrystalExarch = 8650,
+            ThancredsAvatar = 11266,
+            GrahaTiasAvatar = 11271,
+            GrahaTia = 9363,
+        }; */
+
+        private static HashSet<ClassJobType> Tanks = new HashSet<ClassJobType>()
+        {
+            ClassJobType.Gladiator,
+            ClassJobType.Marauder,
+            ClassJobType.Paladin,
+            ClassJobType.Gunbreaker,
+            ClassJobType.Warrior,
+            ClassJobType.DarkKnight,
+        };
+
+        private static HashSet<ClassJobType> DPS = new HashSet<ClassJobType>()
+        {
+
+            ClassJobType.Lancer,		
+            ClassJobType.Archer,
+            ClassJobType.Thaumaturge,
+            ClassJobType.Pugilist,
+            ClassJobType.Monk,		
+            ClassJobType.Dragoon,
+            ClassJobType.Bard,	
+            ClassJobType.BlackMage,
+            ClassJobType.Arcanist,
+            ClassJobType.Summoner,
+            ClassJobType.Rogue,
+            ClassJobType.Ninja,
+            ClassJobType.Machinist,
+            ClassJobType.Samurai,	
+            ClassJobType.RedMage,
+            ClassJobType.Dancer,
+            ClassJobType.Reaper,
+
+        };
+
+        private static HashSet<ClassJobType> Healers = new HashSet<ClassJobType>()
+        {
+
+           ClassJobType.Sage,
+           ClassJobType.Astrologian,
+           ClassJobType.WhiteMage,
+           ClassJobType.Scholar,
+           ClassJobType.Conjurer,
+
+        };
+
+        public static bool IsTank(this BattleCharacter bc)
+        {
+            return Tanks.Contains(bc.CurrentJob) ? true : false;
+        }
+
+        public static bool IsDPS(this BattleCharacter bc)
+        {
+            return DPS.Contains(bc.CurrentJob) ? true : false;
+        }
+
+        public static bool IsHealer(this BattleCharacter bc)
+        {
+            return Healers.Contains(bc.CurrentJob) ? true : false;
+        }
+            
+
+
         /// <summary>
         /// Checks if any nearby <see cref="BattleCharacter"/> is casting any spell ID in this collection.
         /// </summary>
@@ -32,6 +122,31 @@ namespace Trust.Extensions
             return GameObjectManager.GetObjectsOfType<BattleCharacter>(true, false)
                     .Any(obj => spellCastIds.Contains(obj.CastingSpellId) && obj.Distance() < 50);
         }
+
+         public static bool IsCastingtwo(this HashSet<uint> spellCastIds)
+        {
+            var actids = GameObjectManager.GetObjectsOfType<BattleCharacter>()
+               ?.Where(obj => obj.IsCasting &&  !(bool)PartyManager.AllMembers?.Any(p => p.ObjectId == obj.ObjectId));
+
+            if ((bool)actids?.Any())
+            {
+                foreach (var actid in actids)
+                {
+                    Logging.Write(Colors.Yellow, $@" IsCastingtwo 判断显示在使用的 actid ： {actid.CastingSpellId} {actid.SpellCastInfo.IsCasting} {actid.SpellCastInfo.SpellData.LocalizedName} {spellCastIds.Contains(actid.CastingSpellId)}");
+
+                    return spellCastIds.Contains(actid.CastingSpellId) && actid.SpellCastInfo.IsCasting;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool IsFw(this HashSet<uint> bossid)
+        {
+            return (bool)GameObjectManager.GetObjectsOfType<BattleCharacter>(true, false)?
+                    .Any(obj => bossid.Contains(obj.NpcId) && obj.CurrentHealthPercent < 1 && obj.Distance() < 50);
+        }
+
 
         /// <summary>
         /// Follows the specified <see cref="BattleCharacter"/>.
--- Plugins/Trust/Helpers/MovementHelpers.cs
+++ Plugins/Trust/Helpers/MovementHelpers.cs
@@ -1,6 +1,8 @@
 ﻿using Buddy.Coroutines;
+using Clio.Utilities;
 using ff14bot;
 using ff14bot.Behavior;
+using ff14bot.Enums;
 using ff14bot.Helpers;
 using ff14bot.Managers;
 using ff14bot.Navigation;
@@ -47,7 +49,7 @@ namespace Trust.Helpers
             // 11271, // G'raha Tia's avatar
         };
 
-         public static readonly HashSet<uint> AllPartyDpsIds = new HashSet<uint>()
+        public static readonly HashSet<uint> AllPartyDpsIds = new HashSet<uint>()
         {
             729,  // Y'shtola       :: 雅·修特拉
             //1492, // Urianger       :: 于里昂热
@@ -67,7 +69,7 @@ namespace Trust.Helpers
             //10587, // Venat's Phantom
             //10898, // Emet-Selch
             //10899, // Hythlodaeus
-
+            //9363,  G'raha Tia
             //713,  // Thancred       :: 桑克瑞德
             //8650, // Crystal Exarch :: 水晶公
             //11266, // Thancred's avatar
@@ -80,6 +82,7 @@ namespace Trust.Helpers
             8650, // Crystal Exarch :: 水晶公
             11266, // Thancred's avatar
             11271, // G'raha Tia's avatar
+            9363, // G'raha Tia
         };
 
         public static readonly HashSet<uint> AllPartyMemberIds = new HashSet<uint>()
@@ -102,11 +105,68 @@ namespace Trust.Helpers
             10587, // Venat's Phantom
             10898, // Emet-Selch
             10899, // Hythlodaeus
-
+            9363, // G'raha Tia
             713,  // Thancred       :: 桑克瑞德
             8650, // Crystal Exarch :: 水晶公
             11266, // Thancred's avatar
             11271, // G'raha Tia's avatar
+        };
+
+        private static readonly HashSet<ClassJobType> Tanks = new HashSet<ClassJobType>()
+        {
+            ClassJobType.Gladiator,
+            ClassJobType.Marauder,
+            ClassJobType.Paladin,
+            ClassJobType.Gunbreaker,
+            ClassJobType.Warrior,
+            ClassJobType.DarkKnight,
+        };
+
+        private static readonly HashSet<ClassJobType> DPS = new HashSet<ClassJobType>()
+        {
+            ClassJobType.Lancer,
+            ClassJobType.Archer,
+            ClassJobType.Thaumaturge,
+            ClassJobType.Pugilist,
+            ClassJobType.Monk,
+            ClassJobType.Dragoon,
+            ClassJobType.Bard,
+            ClassJobType.BlackMage,
+            ClassJobType.Arcanist,
+            ClassJobType.Summoner,
+            ClassJobType.Rogue,
+            ClassJobType.Ninja,
+            ClassJobType.Machinist,
+            ClassJobType.Samurai,
+            ClassJobType.RedMage,
+            ClassJobType.Dancer,
+            ClassJobType.Reaper,
+        };
+
+        private static readonly HashSet<ClassJobType> Healers = new HashSet<ClassJobType>()
+        {
+           ClassJobType.Sage,
+           ClassJobType.Astrologian,
+           ClassJobType.WhiteMage,
+           ClassJobType.Scholar,
+        };
+
+        private static readonly List<ClassJobType> Melee = new List<ClassJobType>
+        {
+            ClassJobType.Lancer,
+            ClassJobType.Dragoon,
+            ClassJobType.Pugilist,
+            ClassJobType.Monk,
+            ClassJobType.Rogue,
+            ClassJobType.Ninja,
+            ClassJobType.Samurai,
+            ClassJobType.Reaper,
+            ClassJobType.DarkKnight,
+            ClassJobType.Gladiator,
+            ClassJobType.Marauder,
+            ClassJobType.Paladin,
+            ClassJobType.Warrior,
+            ClassJobType.Gunbreaker
         };
 
         /// <summary>
@@ -118,24 +178,51 @@ namespace Trust.Helpers
             .FirstOrDefault();
 
         public static BattleCharacter GetClosestDps => GameObjectManager.GetObjectsOfType<BattleCharacter>(true, false)
-            .Where(obj => !obj.IsDead && AllPartyDpsIds.Contains(obj.NpcId))
+            .Where(obj => !obj.IsDead && AllPartyMemberIds.Contains(obj.NpcId) && DPS.Contains(obj.CurrentJob))
             .OrderBy(r => r.Distance())
             .FirstOrDefault();
 
         public static BattleCharacter GetClosestTank => GameObjectManager.GetObjectsOfType<BattleCharacter>(true, false)
-            .Where(obj => !obj.IsDead && AllPartyTankIds.Contains(obj.NpcId))
+            .Where(obj => !obj.IsDead && AllPartyMemberIds.Contains(obj.NpcId) && Tanks.Contains(obj.CurrentJob))
             .OrderBy(r => r.Distance())
             .FirstOrDefault();
 
+        public static BattleCharacter GetClosestMelee => GameObjectManager.GetObjectsOfType<BattleCharacter>(true, false)
+            .Where(obj => !obj.IsDead && AllPartyMemberIds.Contains(obj.NpcId) && Melee.Contains(obj.CurrentJob))
+            .OrderBy(r => r.Distance())
+            .FirstOrDefault();
+
+        public static Vector3 Vec = new Vector3("0,0,1");
+        public static BattleCharacter GetClosestLocal(Vector3 vector)
+        {
+            if (vector == null) return null;
+
+            return GameObjectManager.GetObjectsOfType<BattleCharacter>(true, false)
+
+            .Where(obj => !obj.IsDead && PartyMemberIds.Contains(obj.NpcId))
+            .OrderBy(r => r.Distance(vector))
+            .FirstOrDefault();
+        }
+            
         /// <summary>
         /// Gets the furthest Ally from the Player.
         /// </summary>
         public static BattleCharacter GetFurthestAlly => GameObjectManager.GetObjectsOfType<BattleCharacter>(true, false)
             .Where(obj => !obj.IsDead && PartyMemberIds.Contains(obj.NpcId))
             .OrderByDescending(r => r.Distance())
-            .FirstOrDefault();     
+            .FirstOrDefault();
 
-        public static async Task<bool> Spread(double TimeToSpread, float spreadDistance = 6.5f, bool IsSpreading = false)
+
+        public static bool IsMelee(this GameObject tar)
+        {
+            var gameObject = tar as Character;
+
+            return gameObject != null && (bool)Melee?.Contains(gameObject.CurrentJob);
+        }
+
+        private static Vector3 PlayerLoc => Core.Player.Location;
+
+        public static async Task<bool> Spread(double TimeToSpread, float spreadDistance = 6.5f, bool IsSpreading = false, uint spbc = 0)
         {
             if (IsSpreading)
             {
@@ -145,31 +232,143 @@ namespace Trust.Helpers
             double CurrentMS = DateTime.Now.TimeOfDay.TotalMilliseconds;
             double EndMS = CurrentMS + (TimeToSpread);
 
+            if (spbc != 0)
+            {
+                AllPartyMemberIds.Add(spbc);
+            }
+
             //if (sidestepPlugin != null)
             //    { 
             //        sidestepPlugin.Enabled = true;
             //    }
-                        
+
             foreach (var npc in GameObjectManager.GetObjectsOfType<BattleCharacter>(true, false)
                                 .Where(obj => AllPartyMemberIds.Contains(obj.NpcId)))
-                {
-                    AvoidanceManager.AddAvoidObject<BattleCharacter>(
-                        () => DateTime.Now.TimeOfDay.TotalMilliseconds <= EndMS,
-                        radius: spreadDistance,
-                        npc.ObjectId);
-                    await Coroutine.Yield();
-                }
-               
+            {
+                AvoidanceManager.AddAvoidObject<BattleCharacter>(
+                    () => DateTime.Now.TimeOfDay.TotalMilliseconds <= EndMS,
+                    radius: spreadDistance,
+                    npc.ObjectId);
+                await Coroutine.Yield();
+            }
+
             if (!AvoidanceManager.IsRunningOutOfAvoid)
-                            {
-                                MovementManager.MoveStop();
-                            }
+            {
+                MovementManager.MoveStop();
+            }
 
             return true;
         }
 
+        public static async Task<bool> SpreadSp(double TimeToSpread, Vector3 vector, float spreadDistance = 6.5f, bool IsSpreading = false, uint spbc = 0)
+        {
+            if (IsSpreading)
+            {
+                return true;
+            }
 
+            double CurrentMS = DateTime.Now.TimeOfDay.TotalMilliseconds;
+            double EndMS = CurrentMS + (TimeToSpread);
+
+            if (spbc != 0)
+            {
+                AllPartyMemberIds.Add(spbc);
+            }
+
+            //if (sidestepPlugin != null)
+            //    { 
+            //        sidestepPlugin.Enabled = true;
+            //    }
+
+            var nobj = GameObjectManager.GetObjectsOfType<BattleCharacter>(true, false)
+                                .Where(obj => AllPartyMemberIds.Contains(obj.NpcId)).OrderBy(obj => obj.Distance(Core.Player)).FirstOrDefault();
+
+            float ls = 0;
+            if (vector != null)
+            {
+                if (PlayerLoc.X > vector.X)
+                {
+                    ls = -20;
+                }
+                else
+                {
+                    ls = 20;
+                }
+            }
+
+
+
+            foreach (var npc in GameObjectManager.GetObjectsOfType<BattleCharacter>(true, false)
+                                .Where(obj => AllPartyMemberIds.Contains(obj.NpcId)).OrderByDescending(r => Core.Player.Distance()))
+            {
+                AvoidanceManager.AddAvoidObject<BattleCharacter>(
+                    () => DateTime.Now.TimeOfDay.TotalMilliseconds <= EndMS,
+                    () => new Vector3(PlayerLoc.X - ls, PlayerLoc.Y, PlayerLoc.Z),
+                    leashRadius: 40,
+                    radius: spreadDistance,
+                    npc.ObjectId);
+
+                await Coroutine.Yield();
+            }
+
+            if (!AvoidanceManager.IsRunningOutOfAvoid)
+            {
+                MovementManager.MoveStop();
+            }
+
+            return true;
+        }
+
+        public static async Task<bool> SpreadSpLoc(double TimeToSpread, Vector3 vector, float spreadDistance = 6.5f, bool IsSpreading = false, uint spbc = 0)
+        {
+            if (IsSpreading)
+            {
+                return true;
+            }
+
+            double CurrentMS = DateTime.Now.TimeOfDay.TotalMilliseconds;
+            double EndMS = CurrentMS + (TimeToSpread);
+
+            if (spbc != 0)
+            {
+                AllPartyMemberIds.Add(spbc);
+            }
+
+            //if (sidestepPlugin != null)
+            //    { 
+            //        sidestepPlugin.Enabled = true;
+            //    }
+
+
+            if (vector == null)
+            {
+                vector = GameObjectManager.GetObjectsOfType<BattleCharacter>(true, false)
+                                .Where(obj => AllPartyMemberIds.Contains(obj.NpcId)).OrderBy(obj => obj.Distance(Core.Player)).FirstOrDefault().Location;
+            }
+
+
+
+            foreach (var npc in GameObjectManager.GetObjectsOfType<BattleCharacter>(true, false)
+                                .Where(obj => AllPartyMemberIds.Contains(obj.NpcId)).OrderByDescending(r => Core.Player.Distance()))
+            {
+                AvoidanceManager.AddAvoidObject<BattleCharacter>(
+                    () => DateTime.Now.TimeOfDay.TotalMilliseconds <= EndMS,
+                    () => vector,
+                    leashRadius: 40,
+                    radius: spreadDistance,
+                    npc.ObjectId);
+
+                await Coroutine.Yield();
+            }
+
+            if (!AvoidanceManager.IsRunningOutOfAvoid)
+            {
+                MovementManager.MoveStop();
+            }
+
+            return true;
+        }
     }
 
-   
+
 }
